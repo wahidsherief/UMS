3e87858d4357b43d738626e6b8694f74964a886a
Joy <joyustcian@gmail.com>
2021-07-11 00:41:08 +0600

    Forget Password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use APp\Models\User;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +28,16 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    protected function redirectTo()
+    {
+        if (Auth()->user()->role == 1) {
+            return route('superadmin.dashboard');
+        } else if (Auth()->user()->role == 2) {
+            return route('admin.dashboard');
+        } else if (Auth()->user()->role == 3) {
+            return route('teacher.dashboard');
+        } else if (Auth()->user()->role == 4) {
+            return route('student.dashboard');
+        }
+    }
 }
